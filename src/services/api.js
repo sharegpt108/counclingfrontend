f50606f52c4034dc4bc9d0ca346e7e1d09a77107
vipinsharma0999 <sharegpt108@gmail.com>
2025-04-04 19:48:43 +0530

    changes with front code

---
+++ src/services/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+// Create an Axios instance with base URL
+const api = axios.create({
+  // baseURL: "https://lakshay-path.onrender.com/api", 
+  // baseURL: "http://localhost:5000/api", 
+    baseURL: "https://counclingbackend.vercel.app/api", 
+  withCredentials: true,
+});
+
+// Response interceptor for handling 401 errors
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      if (
+        window.location.pathname !== "/admin" &&
+        window.location.pathname !== "/"
+      ) {
+        window.location.href = "/admin";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api
